024fca7e133ecb419f1446d2d16cc8fe5766f8c9
cb <cbroot@163.com>
2019-03-05 20:08:14 +0800

    随便问问

---
+++ src/main/java/com/jk/service/TreeService.java
@@ -0,0 +1,16 @@
+package com.jk.service;
+
+import com.jk.bean.Title;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.List;
+
+/**
+ * &lt;pre&gt;${enclosing_method}(这里用一句话描述这个方法的作用)
+ *
+ * @Author：陈斌 创建时间：${date} ${time}
+ * ${tags}&lt;/pre&gt;
+ */
+public interface TreeService {
+ List<Title> getTree();
+}
