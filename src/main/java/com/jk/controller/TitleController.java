6a5e0bb282cf956719eaaca79daf0a70dcefcfdf
chen <2439146769@qq.com>
2019-03-05 19:12:37 +0800

    2222

--- src/main/java/com/jk/controller/TitleController.java
+++ src/main/java/com/jk/controller/TitleController.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("title")
 public class TitleController {
